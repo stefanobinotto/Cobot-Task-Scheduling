640e07d55ff0a044e4af426c1aa8c1abf6ad7982
Stefano Binotto <59732459+stefanobinotto@users.noreply.github.com>
2025-01-29 13:27:24 +0100

    Update replay.py

--- memory/replay.py
+++ memory/replay.py
@@ -9,9 +9,12 @@ class ReplayBuffer:
         """
         Replay Buffer memory to save the state, action, reward sequence from the current episode.
         
-        Args:
-            max_capacity (int): max buffer capacity.
-            device (torch.device or int): device on which to save sampled transitions.
+        Parameters
+        ----------
+            max_capacity: int
+                Max buffer capacity.
+            device: torch.device|int
+                Device on which to save sampled transitions.
         """
         
         self.capacity = max_capacity
@@ -21,17 +24,24 @@ class ReplayBuffer:
         self.buffer = deque(maxlen=self.capacity)
 
 
-    def store(self, state, action, reward, next_state, done) -> None:
+    def push(self, state, action, reward, next_state, done) -> None:
         """
         Store a transition into the buffer.
         
-        Args:
-            state (np.array): current state.
-            action (int): action.
-            reward (float): reward.
-            next_state (np.array): next state.
-            done (bool): terminal flag.
+        Parameters
+        ----------
+            state: np.array
+                Current state.
+            action: int
+                Action.
+            reward: float
+                Reward.
+            next_state: np.array
+                Next state.
+            done: bool
+                Terminal flag.
         """
+        
         assert isinstance(state, np.ndarray), "Invalid state!"
         assert isinstance(action, int), "Invalid action!"
         assert isinstance(reward, float), "Invalid reward!"
@@ -45,11 +55,15 @@ class ReplayBuffer:
         """
         Sample a batch of transitions from the buffer.
         
-        Args:
-            batch_size (int): size of the batch.
+        Parameters
+        ----------
+            batch_size: int
+                Size of the batch.
 
-        Returns:
-            tuple: Batch of transitions.
+        Returns
+        -------
+            tuple
+                Batch of transitions.
         """
 
         assert self.size() >= batch_size, "Replay Buffer not big enough for sampling!"
@@ -65,10 +79,12 @@ class ReplayBuffer:
 
     def size(self) -> int:
         """
-        Returns the current number of elements in the buffer.
+        Current number of elements in the buffer.
         
-        Returns:
-            int: number of stored transitions.
+        Returns
+        -------
+            int
+                Number of stored transitions.
         """
         
         return len(self.buffer)
