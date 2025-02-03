18f740c9b2e4f2ad5a7786930b5742db627ddb07
Stefano Binotto <59732459+stefanobinotto@users.noreply.github.com>
2025-02-03 22:37:41 +0100

    new functions and hyperparams import

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import argparse
 from utils.functions import read_hyperparameters, set_seed
-import Agent
+from agent import Agent
 
 def main():
     # read hyperparams from file using '@' in CLI (e.g. python main.py @args.txt)
     hyperparameters = read_hyperparameters()
-
+    print(hyperparameters["HIDDEN_LAYERS"])
     # deterministic behaviour
     if hyperparameters['SEED'] is not None:
         set_seed(hyperparameters['SEED'])
     
     agent = Agent(hyperparameters)
-    agent.train()
+    if hyperparameters["TRAIN_MODE"]:
+        agent.train(hyperparameters["N_EPISODES"] )
+    else:
+        print("entra qui!")
+        #agent.evaluate()
 
 
 if __name__ == "__main__":
--- utils/functions.py
+++ utils/functions.py
@@ -1,7 +1,11 @@
 import os
 import torch
+import torch.nn as nn
 import random
 import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+import yaml
 
 def set_seed(seed: int = None) -> None:
     """
@@ -25,6 +29,82 @@ def set_seed(seed: int = None) -> None:
         torch.backends.cudnn.benchmark = False     # disable non-deterministic optimisations
 
 
+def plot(scores: list, losses: list, epsilons: list, lrs: list, path: str) -> None:
+    # Creazione della figura e dei sottografi (subplots)
+    fig, axs = plt.subplots(2, 2, figsize=(12, 8))
+    
+    # Grafico 1: Punteggi
+    axs[0, 0].plot(scores, label="Score", color="blue")
+    axs[0, 0].set_title("Score")
+    axs[0, 0].set_xlabel("Episode")
+    axs[0, 0].set_ylabel("Score")
+    axs[0, 0].grid(True)
+    #axs[0, 0].legend()
+    
+    # Grafico 2: Loss
+    axs[0, 1].plot(losses, label="Loss", color="red")
+    axs[0, 1].set_title("Loss")
+    axs[0, 1].set_xlabel("Episode")
+    axs[0, 1].set_ylabel("Loss")
+    axs[0, 1].grid(True)
+    #axs[0, 1].legend()
+    
+    # Grafico 3: Epsilon
+    axs[1, 0].plot(epsilons, label="Epsilon", color="green")
+    axs[1, 0].set_title("Epsilon")
+    axs[1, 0].set_xlabel("Episode")
+    axs[1, 0].set_ylabel("Epsilon")
+    axs[1, 0].grid(True)
+    #axs[1, 0].legend()
+    
+    # Grafico 4: Learning Rate
+    axs[1, 1].plot(lrs, label="Learning Rate", color="orange")
+    axs[1, 1].set_title("Learning Rate")
+    axs[1, 1].set_xlabel("Episode")
+    axs[1, 1].set_ylabel("Learning Rate")
+    axs[1, 1].grid(True)
+    #axs[1, 1].legend()
+    
+    # Miglioriamo il layout e salviamo il grafico
+    plt.tight_layout()
+    plt.savefig(path)
+
+
+def hard_update(main_model: nn.Module, target_model: nn.Module) -> None:
+    """
+    Update target model parameters:
+        
+    θ_target = θ_main
+            
+    Parameters
+    ----------
+        main_model: nn.Module
+            Weights will be copied from.
+        target_model: nn.Module
+            Weights will be copied to.
+    """
+    target_model.load_state_dict(main_model.state_dict())
+    
+    
+def soft_update(main_model: nn.Module, target_model: nn.Module, tau: float) -> None:
+    """
+    Soft update target model parameters:
+        
+    θ_target = τ*θ_main + (1 - τ)*θ_target
+        
+    Parameters
+    ----------
+        main_model: nn.Module
+            Weights will be copied from.
+        target_model: nn.Module
+            Weights will be copied to.
+        tau: float
+            Interpolation parameter.
+    """    
+    for target_param, main_param in zip(target_model.parameters(), main_model.parameters()):
+        target_param.data.copy_(tau*main_param.data + (1.0-tau)*target_param.data)
+
+
 def read_hyperparameters() -> dict:
     """
     Read the hyperparameters from textfile.
@@ -34,49 +114,49 @@ def read_hyperparameters() -> dict:
         dict
             Dictionary of hyperparameters.
     """
-    parser = argparse.ArgumentParser(description="Read hyperparameters")
-
-    # Configuration to read arguments from a file with a specific prefix (e.g. '@')
-    parser.fromfile_prefix_chars = "@"
-
+    with open('hyperparameters.yml', 'r') as file:
+        args = yaml.safe_load(file)
+        
     # Hyperparameter Setting
-    parser.add_argument("--LR", type=float, required=True, help="Learning Rate")
-    parser.add_argument("--LR_STEP_SIZE", type=int, default=None, help="Period of learning rate decay. Ignore if no lr decay is performed")
-    parser.add_argument("--LR_GAMMA", type=float, default=None, help="Multiplicative factor of learning rate decay. Ignore if no lr decayment is performed")
-    parser.add_argument("--BUFFER_SIZE", type=int, required=True, help="Size of the Experience Replay buffer")
-    parser.add_argument("--BATCH_SIZE", type=int, required=True, help="Size of the mini-batch sampled from the Replay buffer")
-    parser.add_argument("--GAMMA", type=float, required=True, help="Discount factor")
-    parser.add_argument("--TAU", type=float, default=None, help="Multiplicative factor for soft update. Ignore if hard update is chosen")
-    parser.add_argument("--EPSILON_START", type=float, required=True, help="Initial epsilon")
-    parser.add_argument("--EPSILON_END", type=float, required=True, help="Final epsilon")
-    parser.add_argument("--EPSILON_DECAY", type=float, required=True, help="Factor of epsilon decayment")
-    parser.add_argument("--STATE_SIZE", type=int, required=True, help="State dimension")
-    parser.add_argument("--ACTION_SIZE", type=int, required=True, help="Number of actions")
-    parser.add_argument("--HIDDEN_LAYERS", nargs="+", type=int, required=True, help="Number of neurons (e.g. 256 128 64)") # nargs="+" per avere una lista da 1 a più elementi
-    parser.add_argument("--SEED", type=int, default=None, help="Set seed if deterministic behaviour is needed, ignore otherwise")
-#    parser.add_argument('--DDQN', type=str2bool, default=True, help='True:DDQN; False:DQN')
-
-    # hyperparameters parsing
-    args = parser.parse_args()
+    #LR, type=float, required=True, help="Learning Rate"
+    #LR_STEP_SIZE, type=int, default=None, help="Period of learning rate decay. If None, no lr decayment is performed"
+    #LR_GAMMA, type=float, default=None, help="Multiplicative factor of learning rate decay. If None, no lr decayment is performed"
+    #BUFFER_SIZE, type=int, required=True, help="Size of the Experience Replay buffer"
+    #BATCH_SIZE, type=int, required=True, help="Size of the mini-batch sampled from the Replay buffer"
+    #GAMMA, type=float, required=True, help="Discount factor"
+    #N_EPISODES, type=int, default=None, help="Number of episodes for training"
+    #HARD_UPDATE_EVERY, type=int, default=None, help="After this number of steps/updates, hard update the target model. If None, soft update is performed"
+    #TAU, type=float, required=True, help="Multiplicative factor for soft update."
+    #EPSILON_START, type=float, required=True, help="Initial epsilon"
+    #EPSILON_END, type=float, required=True, help="Final epsilon"
+    #EPSILON_DECAY, type=float, default=None, help="Factor of epsilon decayment. If None, epsilon decays linearly w.r.t the number of episodes"
+    #STATE_SIZE, type=int, required=True, help="State dimension"
+    #ACTION_SIZE, type=int, required=True, help="Number of actions"
+    #HIDDEN_LAYERS, type=int, nargs="+", required=True, help="Number of neurons (e.g. 256 128 64)") # nargs="+" per avere una lista da 1 a più elementi
+    #SEED, type=int, default=None, help="Set seed if deterministic behaviour is needed, ignore otherwise"
+    #TRAIN_MODE, type=str2bool, default=True, help='True: Train mode; False: Evaluation mode'
+    #BEST_MODEL_PATH, type=str, required=True, help='Path (with extension .pt) for saving the best model'
+    #LOG_PATH, type=str, required=True, help='Path for saving the best model'
+    #PLOT_PATH, type=str, required=True, help='Path for saving the learning curves plot'
     
-    return vars(args)
+    return args
 
 
-def str2bool(v) -> bool:
-    '''
-    Transfer str to bool for argparse:
-    ('yes', 'True','true','TRUE', 't', 'y', '1') -> True
-    ('no', 'False','false','FALSE', 'f', 'n', '0') -> False
+#def str2bool(v) -> bool:
+#    '''
+#    Transfer str to bool for argparse:
+#    ('yes', 'True','true','TRUE', 't', 'y', '1') -> True
+#    ('no', 'False','false','FALSE', 'f', 'n', '0') -> False
 
-    Returns
-    -------
-        bool
-    '''
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes','True','true','TRUE','t','y','1'):
-        return True
-    elif v.lower() in ('no','False','false','FALSE','f','n','0'):
-        return False
-    else:
-        raise TypeError("Only 'yes', 'True', 'true', 'TRUE', 't', 'y', '1' or 'no', 'False', 'false', 'FALSE', 'f', 'n', '0' are allowed!")
+#    Returns
+#    -------
+#        bool
+#    '''
+#    if isinstance(v, bool):
+#        return v
+#    if v.lower() in ('yes','True','true','TRUE','t','y','1'):
+#        return True
+#    elif v.lower() in ('no','False','false','FALSE','f','n','0'):
+#        return False
+#    else:
+#        raise TypeError("Only 'yes', 'True', 'true', 'TRUE', 't', 'y', '1' or 'no', 'False', 'false', 'FALSE', 'f', 'n', '0' are allowed!")
