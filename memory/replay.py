9a0b967a8884938528d9f1dea522b9d6be4a2747
Stefano Binotto <59732459+stefanobinotto@users.noreply.github.com>
2025-02-04 18:35:01 +0100

    Update replay.py

--- memory/replay.py
+++ memory/replay.py
@@ -4,7 +4,6 @@ import torch
 from collections import deque
 
 class ReplayBuffer:
-    #def __init__(self, max_capacity: int, batch_size: int, device: torch.device|str):
     def __init__(self, max_capacity: int, batch_size: int):
         """
         Replay Buffer memory to save the state, action, reward sequence from the current episode.
@@ -17,58 +16,9 @@ class ReplayBuffer:
                 Size of the batch.
         """
         self.capacity = max_capacity
-        self.batch_size = batch_size
-        #self.device = device
-        
+        self.batch_size = batch_size       
         # deque for storing transitions
         self.buffer = deque(maxlen=self.capacity)
-
-
-#    def push(self, state, action, reward, next_state, done) -> None:
-#        """
-#        Store a transition into the buffer.
-#        
-#        Parameters
-#        ----------
-#            state: np.array
-#                Current state.
-#            action: int
-#                Action.
-#            reward: float
-#                Reward.
-#            next_state: np.array
-#                Next state.
-#            done: bool
-#                Terminal flag.
-#        """
-#        assert isinstance(state, torch.float32), "Invalid state!"
-#        assert isinstance(action, int), "Invalid action!"
-#        assert isinstance(reward, float), "Invalid reward!"
-#        assert isinstance(next_state, torch.float32), "Invalid next state!"
-#        assert isinstance(done, bool), "Invalid terminal flag!"
-
-#        self.buffer.append((state, action, reward, next_state, done))
-
-
-#    def sample(self) -> tuple:
-#        """
-#        Sample a batch of transitions from the buffer.
-
-#        Returns
-#        -------
-#            tuple
-#                Batch of transitions.
-#        """
-#        assert self.size() >= self.batch_size, "Replay Buffer not big enough for sampling!"
-        
-#        batch = random.sample(self.buffer, self.batch_size)
-#        states, actions, rewards, next_states, dones = zip(*batch)
-        
- #       return torch.stack(states), \
-  #          torch.stack(actions).unsqueeze(-1), \
-   #         torch.tensor(rewards, dtype=torch.float32, device=self.device).unsqueeze(-1), \
-    #        torch.stack(next_states), \
-     #       torch.tensor(dones, dtype=torch.float32, device=self.device).unsqueeze(-1)
     
     
     def push(self, transition: tuple) -> None:
