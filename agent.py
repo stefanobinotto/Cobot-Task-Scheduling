fb09fc58fb694ed3dbbe46879b7f6bdb869ed26d
Stefano Binotto <59732459+stefanobinotto@users.noreply.github.com>
2025-02-19 17:22:30 +0100

    test function, new epsilon decay, model saving,

--- agent.py
+++ agent.py
@@ -6,25 +6,28 @@ import numpy as np
 from tqdm import tqdm
 import pandas as pd
 import random
-import math
+#import math
+
+from collections import deque
 
 # my packages
-from models.dqn import DQN
-from env.env_v2 import CobotEnv
-from utils.functions import soft_update, hard_update, plot
-from memory.replay import ReplayBuffer
+from models import DQN
+from env import CobotEnv
+from utils import soft_update, hard_update
+from memory import ReplayBuffer
 
 
 class Agent:
     def __init__(self, hyperparameters: dict):
+        
         self.hp = hyperparameters
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        print("Device used: ",self.device)
+        print("Device used:",self.device)
 
         # policy network
-        self.policy_net = DQN(self.hp['STATE_SIZE'], self.hp['HIDDEN_LAYERS'], self.hp['ACTION_SIZE']).to(self.device)
+        self.policy_net = DQN(self.hp['INPUT_SIZE'], self.hp['HIDDEN_LAYERS'], self.hp['ACTION_SIZE']).to(self.device)
         # target network
-        self.target_net = DQN(self.hp['STATE_SIZE'], self.hp['HIDDEN_LAYERS'], self.hp['ACTION_SIZE']).to(self.device)
+        self.target_net = DQN(self.hp['INPUT_SIZE'], self.hp['HIDDEN_LAYERS'], self.hp['ACTION_SIZE']).to(self.device)
         hard_update(self.policy_net, self.target_net)
         for p in self.target_net.parameters(): # freeze parameters
             p.requires_grad = False
@@ -36,7 +39,7 @@ class Agent:
         # reduce lr
         if self.hp['LR_STEP_SIZE'] is not None or self.hp['LR_FACTOR'] is not None:
             # STEP LR
-####            self.scheduler = StepLR(self.optimizer, step_size=self.hp['LR_STEP_SIZE'], gamma=self.hp['LR_FACTOR'])
+            #self.scheduler = StepLR(self.optimizer, step_size=self.hp['LR_STEP_SIZE'], gamma=self.hp['LR_FACTOR'])
             # REDUCE LR ON PLATEAU
             self.scheduler = ReduceLROnPlateau(self.optimizer, factor=self.hp['LR_FACTOR'], patience=self.hp['LR_STEP_SIZE'])
             
@@ -44,30 +47,36 @@ class Agent:
         self.memory = ReplayBuffer(max_capacity=self.hp['BUFFER_SIZE'], batch_size=self.hp['BATCH_SIZE'])
 
 
-    def state_to_flat_tensor(self, state: tuple) -> torch.Tensor:
+    def convert_state(self, state: tuple) -> torch.Tensor:
         """
-        Convert a state (tuple) into a flat tensor.
+        Convert the environment state (tuple) into a flat tensor as the input for model.
 
         Parameters
         ----------
             state: tuple
-                Tuple containing the state of the environment to convert into a tensor.
+                Tuple containing the state of the environment.
 
         Returns
         -------
             torch.float32
-                Flat tansor of the state.          
+                Flat tensor of the state.          
         """
-        s0 = torch.tensor(state[0], dtype=torch.float32)
-        s1 = torch.tensor([state[1]], dtype=torch.float32)
-        s2 = torch.tensor(state[2], dtype=torch.float32)
-        s3 = torch.tensor(state[3], dtype=torch.float32)
-        s4 = torch.tensor([state[4]], dtype=torch.float32)
-        s5 = torch.tensor(state[5], dtype=torch.float32)
-        return torch.cat((s0,s1,s2,s3,s4,s5))
+        # robot cumulative execution time
+        r = np.sum(state[0]*state[2])
+        # operator cumulative execution time
+        t = np.sum(state[3]*state[5])
+
+        s0 = torch.tensor(state[0], dtype=torch.float32)            # robot done
+        s1 = torch.tensor([state[1]], dtype=torch.float32)          # robot scheduled
+        s2 = torch.tensor(state[2], dtype=torch.float32)            # robot execution time
+        s3 = torch.tensor(state[3], dtype=torch.float32)            # operator done
+        s4 = torch.tensor([state[4]], dtype=torch.float32)          # operator scheduled
+        s5 = torch.tensor([max(r,t)-min(r,t)], dtype=torch.float32) # elapsed time of the task still in progress
+        
+        return torch.cat((s0,s1,s2,s3,s4,s5)).to(self.device)
 
     
-    def act(self, state: torch.Tensor, mask: torch.Tensor, epsilon: float = None) -> torch.Tensor:
+    def act(self, state: torch.Tensor, mask: torch.Tensor, epsilon: float) -> torch.Tensor:
         """
         Select a valid (non-masked) action according to an epsilon greedy policy.
 
@@ -78,7 +87,7 @@ class Agent:
             mask: torch.Tensor
                 Mask of valid actions from the given state.
             epsilon: float
-                For epsilon greedy policy, if None -> act deterministically.
+                For epsilon greedy policy; if 0, act greedily.
 
         Returns
         -------
@@ -86,7 +95,7 @@ class Agent:
                 Flat tansor of the state             
         """  
         # select a random action
-        if epsilon is not None and random.random() < epsilon:
+        if random.random() < epsilon:
             valid_actions = torch.where(mask == 1)[0]  # available actions indeces
             action = torch.tensor(random.choice(valid_actions.tolist()), dtype=torch.int64) # tensor(a)            
         # select the greedy action
@@ -96,7 +105,7 @@ class Agent:
                 # we get tensor([[q1], [q2], [q3], [q4]]), so squeeze() to get tensor([q1, q2, q3, q4])
                 # argmax gives tensor(a)
                 self.policy_net.eval()
-                q_values = self.policy_net(state.unsqueeze(dim=0).to(self.device)).squeeze().cpu() # move to cpu to free space
+                q_values = self.policy_net(state.unsqueeze(dim=0)).squeeze().cpu()
                 large = -1000.
                 action = (q_values+large*(1-mask)+large*(1-mask)).argmax()
                 self.policy_net.train()
@@ -104,22 +113,32 @@ class Agent:
     
 
     def learn(self, batch: tuple) -> float:
+        """
+        
+        """
         # separate the single elements of the transitions
         states, actions, rewards, next_states, dones, next_masks = zip(*batch)
         # stack tensors to create batch-like tensors
-        states = torch.stack(states).to(self.device)
-        actions = torch.stack(actions).to(self.device)
-        rewards = torch.stack(rewards).to(self.device)
-        next_states = torch.stack(next_states).to(self.device)
-        dones = torch.stack(dones).to(self.device)
-        next_masks = torch.stack(next_masks).to(self.device)
+        states = torch.stack(states)
+        actions = torch.stack(actions)
+        rewards = torch.stack(rewards)
+        next_states = torch.stack(next_states)
+        dones = torch.stack(dones)
+        next_masks = torch.stack(next_masks)
         
         # compute target Q values
         with torch.no_grad():
-            next_q_values = self.target_net(next_states)
             large = -1000.
-            # masking and maxing
-            max_next_q_value = (next_q_values + large*(1-next_masks) + large*(1-next_masks)).max(dim=1)[0]
+            # Double DQN
+            if self.hp['DOUBLE']:
+                # masking and action selection
+                argmax_a = (self.policy_net(next_states) + large*(1-next_masks) + large*(1-next_masks)).argmax(dim=1)
+                # action evaluation
+                max_next_q_value = self.target_net(next_states).gather(1, index=argmax_a.unsqueeze(dim=1)).squeeze()
+            # DQN
+            else:
+                # masking and maxing
+                max_next_q_value = (self.target_net(next_states) + large*(1-next_masks) + large*(1-next_masks)).max(dim=1)[0]
             # Q target
             target_q = rewards + (1-dones)*self.hp['GAMMA'] * max_next_q_value   # shape [batch size]        
         # compute current Q values
@@ -130,28 +149,36 @@ class Agent:
         self.optimizer.zero_grad()  # clear gradients
         loss.backward()             # compute gradients
         self.optimizer.step()       # update policy network
-        
         return loss.item()
 
     
-    def train(self, n_episodes: int):
-
+    def train(self) -> pd.DataFrame:
+        """
+        
+        """
         # instance of the environment
         env = CobotEnv()
 
         # list to keep track of return, loss, epsilon, lr and model saving collected from every episode
-        scores, losses, epsilons, lrs, saves = [], [], [], [], []
-        # to track best return for model selection
-        best_episode_score = -np.inf
-        update_count = 0
+        scores, losses, epsilons, lrs = [], [], [], []
+
+        # to track best return for model saving
+        score_window = deque(maxlen=10)
+        best_score = -np.inf
+        update_count, epsilon_count = 0, 0
+        
         # initialize epsilon
         epsilon = self.hp['EPSILON_START']
         
-        for episode in tqdm(range(1, n_episodes+1), desc="# of episodes"):
-        #for episode in range(1, n_episodes+1):
-            #print("\rEpisode n°:", episode)
-            state = env.reset()
-            state = self.state_to_flat_tensor(state) # to convert the state tuple to a flat tensor
+        for episode in tqdm(range(self.hp['N_EPISODES'])):
+            # even episode: expert operator
+            #if episode%2 == 0:
+            #    state = env.reset(1)
+            # odd episode: slow operator
+            #else:
+            #    state = env.reset(0)
+            state = env.reset(0)
+            state = self.convert_state(state) # convert from env state to NN input state
 
             episode_score, episode_loss, loss_count = 0.0, 0.0, 0
             done = False
@@ -161,17 +188,17 @@ class Agent:
                 # get valid actions
                 mask = env.get_valid_actions()
                 # select best action, according to epsilon greedy policy and valid actions
-                action = self.act(state, mask, epsilon)
+                action = self.act(state, mask, epsilon).to(self.device)
                 # execute action
                 next_state, reward, done = env.step(action.item() + 1) # +1 makes the action range between 1 and ACTION_SIZE
                 # cumulate reward
                 episode_score += reward
                 # get next state mask
-                next_mask = env.get_valid_actions()
+                next_mask = env.get_valid_actions().to(self.device)
                 # convert next state, reward and done to tensors
-                next_state = self.state_to_flat_tensor(next_state)
-                reward = torch.tensor(reward, dtype=torch.float32)
-                done = torch.tensor(done, dtype=torch.float32)
+                next_state = self.convert_state(next_state) # convert from env state to dqn input state
+                reward = torch.tensor(reward, dtype=torch.float32).to(self.device)
+                done = torch.tensor(done, dtype=torch.float32).to(self.device)
                 # store transition into replay buffer
                 self.memory.push((state, action, reward, next_state, done, next_mask))
 
@@ -179,45 +206,45 @@ class Agent:
                 
                 # if enough samples have been collected into the replay buffer
                 if self.memory.size() >= self.hp['BATCH_SIZE']:
-                    batch = self.memory.sample()
-                    loss = self.learn(batch)
-                    
-                    episode_loss += loss
+                    # update counters
                     loss_count += 1
                     update_count += 1
+                    
+                    # sample batch
+                    batch = self.memory.sample()
+                    # train
+                    loss = self.learn(batch)
+                    episode_loss += loss
 
+                    # update target network
                     if self.hp['HARD_UPDATE_EVERY'] is None:
                         soft_update(self.policy_net, self.target_net, self.hp['TAU'])
                     else:
                         if update_count > self.hp['HARD_UPDATE_EVERY']:
                             update_count = 0
                             hard_update(self.policy_net, self.target_net)
-                        
+
                     # Decay epsilon after every update
-                    if self.hp['EPSILON_DECAY'] is None:
-                        #linear decay
-                        epsilon = self.hp['EPSILON_START'] - (self.hp['EPSILON_START'] - self.hp['EPSILON_END']) * (episode / n_episodes)
-                    else:
-                        # exponential decay
-                        epsilon = self.hp['EPSILON_END'] + (self.hp['EPSILON_START'] - self.hp['EPSILON_END']) * math.exp(-1. * episode / self.hp['EPSILON_DECAY'])
+                    # exponential decay
+                    #epsilon = self.hp['EPSILON_END'] + (self.hp['EPSILON_START'] - self.hp['EPSILON_END']) * math.exp(-1. * episode / self.hp['EPSILON_DECAY'])
+                    # dynamic decay
+                    epsilon = self.hp['EPSILON_END']+(self.hp['EPSILON_START']-self.hp['EPSILON_END'])*max((self.hp['EPS_DECAY_EPISODE']-max(0,epsilon_count))/self.hp['EPS_DECAY_EPISODE'], 0)
             
-            # display the performance every 100 episodes
-#            if episode % 100 == 0:
-#                if loss_count == 0:
-#                    print('\rEpisode {} | Score: {:.2f} | Loss: {:.2f} | LR: {:.5f} | Eps: {:.2f}'\
-#                          .format(episode, episode_score, episode_loss, self.scheduler.get_last_lr()[0], epsilon))
-#                else:
-#                    print('\rEpisode {} | Score: {:.2f} | Loss: {:.2f} | LR: {:.5f} | Eps: {:.2f}'\
-#                          .format(episode, episode_score, episode_loss/loss_count, self.scheduler.get_last_lr()[0], epsilon))
-            
-            # save model if episode
-            if episode_score != 0.0 and episode_score > best_episode_score:
-                best_episode_score = episode_score
-                saves.append("Saved")
-                #print('\rSaving model - Episode {} - Score: {:.2f}'.format(episode, episode_score))
-                torch.save(self.policy_net.state_dict(), self.hp['BEST_MODEL_PATH'])
-            else:
-                saves.append("")
+            # if at least one backprop swept has been performed
+            if loss_count > 0:
+                epsilon_count += 1
+
+                score_window.append(episode_score)
+                # save best model
+                if np.mean(score_window) > best_score:
+                    # update best score
+                    best_score = np.mean(score_window)
+                    # save model
+                    torch.save({
+                        "best_score": best_score,
+                        "episode": episode,
+                        "model_state_dict": self.policy_net.state_dict()
+                    }, self.hp['LOG_PATH']+"checkpoint.pt")
 
             # training logs
             scores.append(episode_score)
@@ -231,7 +258,7 @@ class Agent:
                 lrs.append(self.scheduler.get_last_lr()[0])
                 self.scheduler.step(episode_loss/loss_count)
             else:
-                lrs.append(self.hp['LR'])
+                lrs.append(self.hp['LR'])       
 
         # save training logs
         data = {
@@ -239,18 +266,40 @@ class Agent:
             "Score": scores,
             "Loss": losses,
             "Epsilon": epsilons,
-            "Learning rate": lrs,
-            "Model saving": saves
+            "Learning rate": lrs
         }
-        df = pd.DataFrame(data)
-        df.to_csv(self.hp['LOG_PATH'], index=False)
-        plot(df, self.hp['PLOT_PATH'])
-        print(f"\rBest episode score: {best_episode_score}.") 
-        print(f"Dati salvati in {self.hp['LOG_PATH']} e plottati in {self.hp['PLOT_PATH']}.") 
-                
+        return pd.DataFrame(data)
+    
 
-    def evaluate(self, model, env, n_eval_episodes):
-        """
-        Qui dentro eseguire n_eval_episodes episodi e restituire mean return e std return
-        """
-        #prendere path del modello salvato, load del modello, creazione env, esecuzione degli episodi e calcolo metriche
+    def test(self, n_runs: int = 200):
+        # load checkpoint
+        checkpoint = torch.load(self.hp['MODEL_PATH'], weights_only=False)
+        # load model weights
+        self.policy_net.load_state_dict(checkpoint['model_state_dict'])
+        self.policy_net.eval()
+
+        best_score = checkpoint['best_score']
+        episode = checkpoint['episode']
+        print('\rModel saved - Episode {} - Score (SMA): {:.2f}'.format(episode, best_score))
+
+        env = CobotEnv()
+        scores = []
+
+        for _ in range(n_runs):
+            state = env.reset(0)
+            episode_score = 0.0
+            done = False
+            while not done:
+                # get valid actions
+                mask = env.get_valid_actions()
+                # act greedily
+                action = self.act(self.convert_state(state), mask, epsilon=0).to(self.device)
+                self.policy_net.eval()
+                # execute action
+                state, reward, done = env.step(action.item() + 1) # +1 makes the action range between 1 and ACTION_SIZE
+                # cumulate reward
+                episode_score += reward
+            scores.append(episode_score)
+            
+        print("Mean Score:",np.mean(scores))
+        print("Std Score:",np.std(scores))
