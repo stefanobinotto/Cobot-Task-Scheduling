c0ce161973be968624c933c4703e67138de7edf6
Stefano Binotto <59732459+stefanobinotto@users.noreply.github.com>
2025-02-19 17:27:23 +0100

    logs saving & test function

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
-from utils.functions import read_hyperparameters, set_seed
+from utils import read_hyperparameters, set_seed, plot
 from agent import Agent
+import torch
+
 
 def main():
     # import hyperparameters
     hyperparameters = read_hyperparameters()
-    # deterministic behaviour
-    if hyperparameters['SEED'] is not None:
-        set_seed(hyperparameters['SEED'])
+
+    ### TEST
     
-    agent = Agent(hyperparameters)
-    if hyperparameters["TRAIN_MODE"]:
-        agent.train(hyperparameters["N_EPISODES"] )
-    else:
-        print("entra qui!")
-        #agent.evaluate()
+    if hyperparameters['MODEL_PATH'] is not None:
+        agent = Agent(hyperparameters)
+        agent.test()
+        exit(0)
+
+    ### TRAIN
+    
+    # deterministic behaviour
+    if hyperparameters['SEED'] is not None and hyperparameters['N_EXPERIMENTS'] == 1:
+        set_seed(hyperparameters['SEED'])
+        print("Setting seed!")
+
+    all_scores = []
+    all_losses = []
+    for i in range(hyperparameters['N_EXPERIMENTS']):
+        agent = Agent(hyperparameters)
+        logs = agent.train()
+        logs.to_csv(hyperparameters['LOG_PATH']+str(i+1)+"_run.csv", index=False)
+        all_scores.append(logs['Score'])
+        all_losses.append(logs['Loss'])
+        # free CUDA memory
+        del agent
+        torch.cuda.empty_cache()
+
+    plot(all_scores, all_losses, logs['Epsilon'], hyperparameters['LOG_PATH']+"curves.png")  
 
 
 if __name__ == "__main__":
